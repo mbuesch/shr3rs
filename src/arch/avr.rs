1c86a06895637a44a90d492bfc5120eefd957be6
Michael Buesch <m@bues.ch>
2022-06-15 21:26:14 +0200

    arch/avr: Add more comments

--- src/arch/avr.rs
+++ src/arch/avr.rs
@@ -12,16 +12,20 @@ use core::arch::asm;
 #[cfg_attr(feature="__devmode__", inline(never))]
 #[cfg_attr(not(feature="__devmode__"), inline(always))]
 pub fn shr3(state: u32) -> u32 {
+    // Split state to 16 bit pairs. The compiler optimizes this away.
     let mut ab = state as u16;
     let mut cd = (state >> 16) as u16;
 
     unsafe {
+        // Cycles: 2 + 15 + 5 + 20 + 2 = 44
         asm!(
             // Setup
+            // Cycles: 2
             "mov    {r0_save}, r0",                     // save r0
             "ldi    {fac}, 32",                         // mul factor
 
             // y ^= y << 13
+            // Cycles: 1 + 1 + 2 + 1 + 2 + (2 * 1) + 2 + (4 * 1) = 15
             "movw   {tab:l}:{tab:h}, {ab:l}:{ab:h}",    // mov ab to temp
             "mov    {tcd:l}, {cd:l}",                   // mov c to temp
             "mul    {tcd:l}, {fac}",                    // r0:r1 = c << 5
@@ -36,6 +40,7 @@ pub fn shr3(state: u32) -> u32 {
             "eor    {cd:h}, {tcd:l}",                   // d ^= temp_c << 8
 
             // y ^= y >> 17
+            // Cycles: 5
             "movw   {tcd:l}:{tcd:h}, {cd:l}:{cd:h}",    // mov cd to temp
             "lsr    {tcd:h}",                           // temp_d >>= 1
             "ror    {tcd:l}",                           // temp_c >>= 1
@@ -43,6 +48,7 @@ pub fn shr3(state: u32) -> u32 {
             "eor    {ab:h}, {tcd:h}",                   // b ^= temp_d >> 16
 
             // y ^= y << 5
+            // Cycles: (2 * 1) + 2 + 1 + 2 + (2 * 1) + 2 + (2 * 1) + 2 + (5 * 1) = 20
             "movw   {tab:l}:{tab:h}, {ab:l}:{ab:h}",    // mov ab to temp
             "movw   {tcd:l}:{tcd:h}, {cd:l}:{cd:h}",    // mov cd to temp
             "mul    {tcd:h}, {fac}",                    // r0:r1 = d << 5
@@ -61,6 +67,7 @@ pub fn shr3(state: u32) -> u32 {
             "eor    {cd:h}, {tcd:h}",                   // d ^= temp_d
 
             // Cleanup
+            // Cycles: 2
             "clr    r1",                                // restore r1
             "mov    r0, {r0_save}",                     // restore r0
 
@@ -71,10 +78,11 @@ pub fn shr3(state: u32) -> u32 {
             fac = out(reg_upper) _,                     // mul factor
             r0_save = out(reg) _,                       // r0 restore
 
-            options(pure, nomem, nostack),              // only register ops
+            options(pure, nomem, nostack),              // We only access registers
         );
     }
 
+    // Combine the 16 bit state pairs. The compiler optimizes this away.
     (ab as u32) | ((cd as u32) << 16)
 }
 
