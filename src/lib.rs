77f6a13e196061d6bfb6742957e518ac1b5b5425
Michael Buesch <m@bues.ch>
2022-06-23 21:01:16 +0200

    Add support for signed integers

--- src/lib.rs
+++ src/lib.rs
@@ -69,6 +69,7 @@ use core::ops::{
     ShlAssign,
     Sub,
 };
+use core::num::Wrapping;
 
 /// One round of the SHR3 shuffle function.
 ///
@@ -127,8 +128,11 @@ impl Default for Shr3 {
 }
 
 /// Internal trait for basic operations on the output type.
-pub trait BaseOps: Copy + PartialOrd + Sub<Output=Self> + Add<Output=Self>
+pub trait BaseOps: Copy
 {
+    /// Unsigned companion of Self.
+    type U;
+
     /// Number of bits in type `Self`.
     const NUMBITS: u8;
     /// Smallest possible value of type `Self`.
@@ -139,6 +143,11 @@ pub trait BaseOps: Copy + PartialOrd + Sub<Output=Self> + Add<Output=Self>
     /// Convert an `u8` to `Self`.
     fn from_u8(v: u8) -> Self;
 
+    /// Convert a wrapping unsigned to a bit identical Self.
+    fn from_unsigned(v: Wrapping<Self::U>) -> Self;
+    /// Convert self to a bit identical wrapping unsigned.
+    fn into_unsigned(&self) -> Wrapping<Self::U>;
+
     /// Find last bit set in `self`.
     ///
     /// Bit 0 -> returns 1; Bit 1 -> returns 2; ...
@@ -147,35 +156,71 @@ pub trait BaseOps: Copy + PartialOrd + Sub<Output=Self> + Add<Output=Self>
     fn fls(&self) -> u8;
 }
 
-macro_rules! impl_shr3_types {
-    ($($t:ty),*) => {
+macro_rules! impl_base_ops {
+    // $u = unsigned type.
+    // $s = companion signed type.
+    ($( ($u:ty, $s:ty) ),*) => {
         $(
-            impl BaseOps for $t {
-                const NUMBITS: u8 = <$t>::BITS as u8;
-                const MINVAL: $t = <$t>::MIN;
-                const MAXVAL: $t = <$t>::MAX;
+            impl BaseOps for $u {
+                type U = $u;
+                const NUMBITS: u8 = <$u>::BITS as u8;
+                const MINVAL: $u = <$u>::MIN;
+                const MAXVAL: $u = <$u>::MAX;
                 #[inline]
-                fn from_u8(v: u8) -> $t {
-                    v as $t
+                fn from_u8(v: u8) -> $u {
+                    v as $u
+                }
+                #[inline]
+                fn from_unsigned(v: Wrapping<Self::U>) -> Self {
+                    v.0
+                }
+                #[inline]
+                fn into_unsigned(&self) -> Wrapping<Self::U> {
+                    Wrapping(*self)
                 }
                 #[inline]
                 fn fls(&self) -> u8 {
-                    (<$t>::BITS - self.leading_zeros()) as u8
+                    (<$u>::BITS - self.leading_zeros()) as u8
+                }
+            }
+            impl BaseOps for $s {
+                type U = $u;
+                const NUMBITS: u8 = <$s>::BITS as u8;
+                const MINVAL: $s = <$s>::MIN;
+                const MAXVAL: $s = <$s>::MAX;
+                #[inline]
+                fn from_u8(v: u8) -> $s {
+                    v as $s
+                }
+                #[inline]
+                fn from_unsigned(v: Wrapping<Self::U>) -> Self {
+                    v.0 as Self
+                }
+                #[inline]
+                fn into_unsigned(&self) -> Wrapping<Self::U> {
+                    Wrapping(*self as Self::U)
+                }
+                #[inline]
+                fn fls(&self) -> u8 {
+                    (<$s>::BITS - self.leading_zeros()) as u8
                 }
             }
         )*
     }
 }
 
-impl_shr3_types!(u8, u16, u32, u64, usize);
+impl_base_ops!((u8, i8), (u16, i16), (u32, i32), (u64, i64), (usize, isize));
 #[cfg(has_u128)]
-impl_shr3_types!(u128);
+impl_base_ops!((u128, i128));
 
 /// Main operations for extracting bits from SHR3 generator.
 ///
-/// The type `T` can be either of `u8`, `u16`, `u32`, `u64`, `u128` or `usize`.
+/// The type `T` can be either of
+/// `u8`, `i8`, `u16`, `i16`, `u32`, `i32`, `u64`, `i64`, `u128`, `i128`, `usize` or `isize`.
 pub trait Shr3Ops<T>:
-    where T: BaseOps
+    where T: BaseOps + Sub<Output=T> + PartialOrd,
+          T::U: BaseOps,
+          Wrapping<T::U>: Sub<Output=Wrapping<T::U>> + Add<Output=Wrapping<T::U>> + PartialOrd,
 {
     /// Get a number of `bitcount` bits from SHR3 and store them in the lower
     /// bits of the returned type `T`.
@@ -198,15 +243,15 @@ pub trait Shr3Ops<T>:
     ///        be bigger to ensure an even distribution of the returned values.
     fn get_minmax(&mut self, min_value: T, max_value: T) -> T {
         debug_assert!(max_value >= min_value);
-        let top = max_value - min_value;
-        let num_bits = top.fls();
+        let range = max_value.into_unsigned() - min_value.into_unsigned();
+        let num_bits = range.0.fls();
         let value = loop {
-            let value = self.get_bits(num_bits);
-            if value <= top {
+            let value = self.get_bits(num_bits).into_unsigned();
+            if value <= range {
                 break value;
             }
         };
-        value + min_value
+        T::from_unsigned(value + min_value.into_unsigned())
     }
 
     /// Get enough bits to construct a random value in the range between `0` and `max_value`.
@@ -243,17 +288,19 @@ pub trait Shr3Ops<T>:
 
 /// Shr3Ops for struct Shr3.
 impl<T> Shr3Ops<T> for Shr3
-    where T: BaseOps + ShlAssign<u8> + BitOrAssign,
+    where T: BaseOps + Sub<Output=T> + PartialOrd,
+          T::U: BaseOps,
+          Wrapping<T::U>: Sub<Output=Wrapping<T::U>> + Add<Output=Wrapping<T::U>> + PartialOrd + ShlAssign<usize> + BitOrAssign,
 {
     fn get_bits(&mut self, bitcount: u8) -> T {
         debug_assert!(bitcount <= T::NUMBITS);
-        let mut ret = T::from_u8(0);
+        let mut ret = T::from_u8(0).into_unsigned();
         for _ in 0..bitcount {
             self.state = shr3(self.state);
             ret <<= 1;
-            ret |= T::from_u8(self.state as u8 & 1);
+            ret |= T::from_u8(self.state as u8 & 1).into_unsigned();
         }
-        ret
+        T::from_unsigned(ret)
     }
 }
 
@@ -273,6 +320,7 @@ mod tests {
 
     #[test]
     fn test_base_ops() {
+        // unsigned
         assert_eq!(u8::NUMBITS, 8);
         assert_eq!(u16::NUMBITS, 16);
         assert_eq!(u32::NUMBITS, 32);
@@ -281,6 +329,16 @@ mod tests {
         #[cfg(has_u128)]
         assert_eq!(u128::NUMBITS, 128);
 
+        // signed
+        assert_eq!(i8::NUMBITS, 8);
+        assert_eq!(i16::NUMBITS, 16);
+        assert_eq!(i32::NUMBITS, 32);
+        assert_eq!(i64::NUMBITS, 64);
+        assert_eq!(isize::NUMBITS, usize::BITS as u8);
+        #[cfg(has_u128)]
+        assert_eq!(i128::NUMBITS, 128);
+
+        // unsigned
         assert_eq!(u8::MINVAL, 0);
         assert_eq!(u16::MINVAL, 0);
         assert_eq!(u32::MINVAL, 0);
@@ -289,6 +347,16 @@ mod tests {
         #[cfg(has_u128)]
         assert_eq!(u128::MINVAL, 0);
 
+        // signed
+        assert_eq!(i8::MINVAL, i8::MIN);
+        assert_eq!(i16::MINVAL, i16::MIN);
+        assert_eq!(i32::MINVAL, i32::MIN);
+        assert_eq!(i64::MINVAL, i64::MIN);
+        assert_eq!(isize::MINVAL, isize::MIN);
+        #[cfg(has_u128)]
+        assert_eq!(i128::MINVAL, i128::MIN);
+
+        // unsigned
         assert_eq!(u8::MAXVAL, u8::MAX);
         assert_eq!(u16::MAXVAL, u16::MAX);
         assert_eq!(u32::MAXVAL, u32::MAX);
@@ -297,6 +365,16 @@ mod tests {
         #[cfg(has_u128)]
         assert_eq!(u128::MAXVAL, u128::MAX);
 
+        // signed
+        assert_eq!(i8::MAXVAL, i8::MAX);
+        assert_eq!(i16::MAXVAL, i16::MAX);
+        assert_eq!(i32::MAXVAL, i32::MAX);
+        assert_eq!(i64::MAXVAL, i64::MAX);
+        assert_eq!(isize::MAXVAL, isize::MAX);
+        #[cfg(has_u128)]
+        assert_eq!(i128::MAXVAL, i128::MAX);
+
+        // unsigned
         assert_eq!(u8::from_u8(42), 42);
         assert_eq!(u16::from_u8(42), 42);
         assert_eq!(u32::from_u8(42), 42);
@@ -305,6 +383,61 @@ mod tests {
         #[cfg(has_u128)]
         assert_eq!(u128::from_u8(42), 42);
 
+        // signed
+        assert_eq!(i8::from_u8(42), 42);
+        assert_eq!(i16::from_u8(42), 42);
+        assert_eq!(i32::from_u8(42), 42);
+        assert_eq!(i64::from_u8(42), 42);
+        assert_eq!(isize::from_u8(42), 42);
+        #[cfg(has_u128)]
+        assert_eq!(i128::from_u8(42), 42);
+
+        // signed
+        assert_eq!(i8::from_u8(0xFF), -1);
+        assert_eq!(i16::from_u8(0xFF), 0xFF);
+        assert_eq!(i32::from_u8(0xFF), 0xFF);
+        assert_eq!(i64::from_u8(0xFF), 0xFF);
+        assert_eq!(isize::from_u8(0xFF), 0xFF);
+        #[cfg(has_u128)]
+        assert_eq!(i128::from_u8(0xFF), 0xFF);
+
+        // from_unsigned (unsigned)
+        assert_eq!(u8::from_unsigned(Wrapping(0xF0_u8)), 0xF0);
+        assert_eq!(u16::from_unsigned(Wrapping(0xF0_u16)), 0xF0);
+        assert_eq!(u32::from_unsigned(Wrapping(0xF0_u32)), 0xF0);
+        assert_eq!(u64::from_unsigned(Wrapping(0xF0_u64)), 0xF0);
+        assert_eq!(usize::from_unsigned(Wrapping(0xF0_usize)), 0xF0);
+        #[cfg(has_u128)]
+        assert_eq!(u128::from_unsigned(Wrapping(0xF0_u128)), 0xF0);
+
+        // from_unsigned (signed)
+        assert_eq!(i8::from_unsigned(Wrapping(0xF0_u8)), -16);
+        assert_eq!(i16::from_unsigned(Wrapping(0xFFF0_u16)), -16);
+        assert_eq!(i32::from_unsigned(Wrapping(0xFFFF_FFF0_u32)), -16);
+        assert_eq!(i64::from_unsigned(Wrapping(0xFFFF_FFFF_FFFF_FFF0_u64)), -16);
+        assert_eq!(isize::from_unsigned(Wrapping((-16_isize) as usize)), -16);
+        #[cfg(has_u128)]
+        assert_eq!(i128::from_unsigned(Wrapping((-16_i128) as u128)), -16);
+
+        // into_unsigned (unsigned)
+        assert_eq!(0xF0_u8.into_unsigned(), Wrapping(0xF0_u8));
+        assert_eq!(0xF0_u16.into_unsigned(), Wrapping(0xF0_u16));
+        assert_eq!(0xF0_u32.into_unsigned(), Wrapping(0xF0_u32));
+        assert_eq!(0xF0_u64.into_unsigned(), Wrapping(0xF0_u64));
+        assert_eq!(0xF0_usize.into_unsigned(), Wrapping(0xF0_usize));
+        #[cfg(has_u128)]
+        assert_eq!(0xF0_u128.into_unsigned(), Wrapping(0xF0_u128));
+
+        // into_unsigned (signed)
+        assert_eq!(-16_i8.into_unsigned(), Wrapping(0xF0_u8));
+        assert_eq!(-16_i16.into_unsigned(), Wrapping(0xFFF0_u16));
+        assert_eq!(-16_i32.into_unsigned(), Wrapping(0xFFFF_FFF0_u32));
+        assert_eq!(-16_i64.into_unsigned(), Wrapping(0xFFFF_FFFF_FFFF_FFF0_u64));
+        assert_eq!(-16_isize.into_unsigned(), Wrapping((-16_isize) as usize));
+        #[cfg(has_u128)]
+        assert_eq!(-16_i128.into_unsigned(), Wrapping((-16_i128) as u128));
+
+        // unsigned fls
         assert_eq!(0x00_u8.fls(), 0);
         assert_eq!(0x80_u8.fls(), 8);
         assert_eq!(0x4F_u8.fls(), 7);
@@ -315,6 +448,18 @@ mod tests {
         assert_eq!(0x4000_0000_0000_0000_u64.fls(), 63);
         #[cfg(has_u128)]
         assert_eq!(0x4000_0000_0000_0000_0000_0000_0000_0000_u128.fls(), 127);
+
+        // signed fls
+        assert_eq!(0x00_i8.fls(), 0);
+        assert_eq!((-127_i8).fls(), 8);
+        assert_eq!(0x4F_i8.fls(), 7);
+        assert_eq!(0x02_i8.fls(), 2);
+        assert_eq!(0x01_i8.fls(), 1);
+        assert_eq!(0x4000_i16.fls(), 15);
+        assert_eq!(0x4000_0000_i32.fls(), 31);
+        assert_eq!(0x4000_0000_0000_0000_i64.fls(), 63);
+        #[cfg(has_u128)]
+        assert_eq!(0x4000_0000_0000_0000_0000_0000_0000_0000_i128.fls(), 127);
     }
 
     #[test]
@@ -331,6 +476,7 @@ mod tests {
 
     #[test]
     fn test_types() {
+        // unsigned
         {
             let mut a = Shr3::new_state(42);
             let b: u8 = a.get_bits(5);
@@ -362,10 +508,44 @@ mod tests {
             let b: u128 = a.get_bits(5);
             assert_eq!(b, 4);
         }
+
+        // signed
+        {
+            let mut a = Shr3::new_state(42);
+            let b: i8 = a.get_bits(5);
+            assert_eq!(b, 4);
+        }
+        {
+            let mut a = Shr3::new_state(42);
+            let b: i16 = a.get_bits(5);
+            assert_eq!(b, 4);
+        }
+        {
+            let mut a = Shr3::new_state(42);
+            let b: i32 = a.get_bits(5);
+            assert_eq!(b, 4);
+        }
+        {
+            let mut a = Shr3::new_state(42);
+            let b: i64 = a.get_bits(5);
+            assert_eq!(b, 4);
+        }
+        {
+            let mut a = Shr3::new_state(42);
+            let b: isize = a.get_bits(5);
+            assert_eq!(b, 4);
+        }
+        #[cfg(has_u128)]
+        {
+            let mut a = Shr3::new_state(42);
+            let b: i128 = a.get_bits(5);
+            assert_eq!(b, 4);
+        }
     }
 
     #[test]
     fn test_get_bits() {
+        // unsigned
         let mut a = Shr3::new_state(42);
         for exp in [0x0001, 0x0000, 0x0001, 0x0005, 0x0001, 0x0004] {
             let b: u16 = a.get_bits(3);
@@ -373,19 +553,37 @@ mod tests {
         }
         let b: u16 = a.get_bits(0);
         assert_eq!(b, 0);
+
+        // signed
+        let mut a = Shr3::new_state(42);
+        for exp in [0x0001, 0x0000, 0x0001, 0x0005, 0x0001, 0x0004] {
+            let b: i16 = a.get_bits(3);
+            assert_eq!(b, exp);
+        }
+        let b: i16 = a.get_bits(0);
+        assert_eq!(b, 0);
     }
 
     #[test]
     fn test_get() {
+        // unsigned
         let mut a = Shr3::new_state(42);
         for exp in [0x20D3, 0x2C5C, 0x2A17, 0xD3C5, 0xAF08, 0x9E5B] {
             let b: u16 = a.get();
             assert_eq!(b, exp);
         }
+
+        // signed
+        let mut a = Shr3::new_state(42);
+        for exp in [0x20D3, 0x2C5C, 0x2A17, 0xD3C5, 0xAF08, 0x9E5B] {
+            let b: i16 = a.get();
+            assert_eq!(b as u16, exp as u16);
+        }
     }
 
     #[test]
     fn test_max() {
+        // unsigned
         let mut a = Shr3::new_state(42);
         for _ in 0..1000 {
             let b: u32 = a.get_max(100);
@@ -393,10 +591,20 @@ mod tests {
         }
         let b: u32 = a.get_max(0);
         assert_eq!(b, 0);
+
+        // signed
+        let mut a = Shr3::new_state(42);
+        for _ in 0..1000 {
+            let b: i32 = a.get_max(100);
+            assert!(b <= 100);
+        }
+        let b: i32 = a.get_max(0);
+        assert!(b <= 0);
     }
 
     #[test]
     fn test_minmax() {
+        // unsigned
         let mut a = Shr3::new_state(42);
         for _ in 0..1000 {
             let b: u32 = a.get_minmax(60, 170);
@@ -404,11 +612,22 @@ mod tests {
         }
         let b: u32 = a.get_minmax(111, 111);
         assert_eq!(b, 111);
+
+        // signed
+        let mut a = Shr3::new_state(42);
+        for _ in 0..1000 {
+            let b: i32 = a.get_minmax(-170, 60);
+            assert!((-170..=60).contains(&b));
+        }
+        let b: i32 = a.get_minmax(-111, -111);
+        assert_eq!(b, -111);
     }
 
     #[test]
     fn test_range() {
         let mut a = Shr3::new_state(42);
+
+        // unsigned
         for _ in 0..1000 {
             let b: u32 = a.get_range(60..170);
             assert!((60..170).contains(&b));
@@ -431,11 +650,39 @@ mod tests {
         }
         let b: u32 = a.get_range(111..112);
         assert_eq!(b, 111);
+        let b: u32 = a.get_range(111..=111);
+        assert_eq!(b, 111);
+
+        // signed
+        for _ in 0..1000 {
+            let b: i32 = a.get_range(-60..170);
+            assert!((-60..170).contains(&b));
+        }
+        for _ in 0..1000 {
+            let b: i32 = a.get_range(-60..=170);
+            assert!((-60..=170).contains(&b));
+        }
+        for _ in 0..1000 {
+            let b: i32 = a.get_range(..170);
+            assert!(b < 170);
+        }
+        for _ in 0..1000 {
+            let b: i32 = a.get_range(..=170);
+            assert!(b <= 170);
+        }
+        for _ in 0..1000 {
+            let b: i32 = a.get_range(0x7FFF_FFF0..);
+            assert!(b >= 0x7FFF_FFF0);
+        }
+        let b: i32 = a.get_range(-111..-110);
+        assert_eq!(b, -111);
+        let b: i32 = a.get_range(-111..=-111);
+        assert_eq!(b, -111);
     }
 
 /*
     #[test]
-    fn test_cycle() {
+    fn test_cycle_unsigned() {
         let seed = 42;
         let mut a = Shr3::new_state(seed);
         let mut first = 0;
@@ -454,6 +701,31 @@ mod tests {
         let _: u32 = a.get_bits(1);
         assert_eq!(a.state, first);
         let _: u32 = a.get_bits(1);
+        assert_eq!(a.state, second);
+    }
+*/
+
+/*
+    #[test]
+    fn test_cycle_signed() {
+        let seed = 42;
+        let mut a = Shr3::new_state(seed);
+        let mut first = 0;
+        let mut second = 0;
+        for i in 0..=u32::MAX-2 {
+            let _: i32 = a.get_bits(1);
+            assert_ne!(a.state, seed);
+            match i {
+                0 => first = a.state,
+                1 => second = a.state,
+                _ => (),
+            }
+        }
+        let _: i32 = a.get_bits(1);
+        assert_eq!(a.state, seed);
+        let _: i32 = a.get_bits(1);
+        assert_eq!(a.state, first);
+        let _: i32 = a.get_bits(1);
         assert_eq!(a.state, second);
     }
 */
